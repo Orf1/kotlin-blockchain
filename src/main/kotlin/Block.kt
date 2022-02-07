771528e92afcf8523fc96b37e67d967d43efcb96
Orfeas <orfeas126@gmail.com>
2022-02-07 09:58:37 -0800

    Added Block.kt

---
+++ src/main/kotlin/Block.kt
@@ -0,0 +1,31 @@
+import java.security.MessageDigest
+
+class Block (val data: String, val previousHash: String, val timeStamp: Long) {
+    var hash: String = hash(this.toString())
+    var nonce = 0
+
+    // Builds String used when hashing block.
+    override fun toString(): String {
+        return data + previousHash + timeStamp.toString() + nonce
+    }
+
+    // Finds the SHA-256 hash of a String.
+    private fun hash(toHash: String): String {
+        val bytes = toHash.toByteArray()
+        val md = MessageDigest.getInstance("SHA-256")
+        val digest = md.digest(bytes)
+        return digest.fold("") { str, it -> str + "%02x".format(it) }
+    }
+
+    // Mines block to specified prefix.
+    fun mine(prefix: String): String {
+        while (!hash.startsWith(prefix)) {
+            nonce++
+            hash = hash(this.toString())
+            if(hash.startsWith("0000")) {
+                println(hash)
+            }
+        }
+        return hash
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
 fun main() {
+    val block = Block("Hello Blockchain!", "null", System.currentTimeMillis())
 
+    println("Un-mined block hash: ${block.hash}")
+
+    println("Attempting to mine block.")
+    block.mine("00000")
+    println("Successfully mined block!")
+
+    println("Mined block hash: ${block.hash}")
 }
